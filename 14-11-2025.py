ea9730a971a6419a218e560cf8f61998a0e27d92
Poojitha@368 <poojithareddy2662004@gmail.com>
2025-11-14 20:10:46 +0530

    power finding using bit manipulation

--- 14-11-2025.py
+++ 14-11-2025.py
@@ -8,3 +8,17 @@ while exp!=1:
     base=base*base
 
 print(base*res)
+
+
+base=5
+exp=2
+res=1
+while exp!=1:
+    if exp&1:
+        res*=2
+    exp = exp>>1
+    print("exp",exp)
+    base=base*base
+    print("base",base)
+
+print(base*res)
